2eebe52efc3822177c83526b57ccc3024a603688
Maxx0909 <maxime.salle09@gmail.com>
2024-02-28 11:47:41 +0100

    modification of build settings

--- app/src/main/java/pack/plantecompagnon/src/User.java
+++ app/src/main/java/pack/plantecompagnon/src/User.java
@@ -1,2 +1,39 @@
-package pack.plantecompagnon.src;public class User {
+package pack.plantecompagnon.src;
+
+import java.util.Date;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    private String pseudo;
+    private String email;
+    private Date birthDate;
+    private int age;
+    private String password;
+    private String Gender;
+    private String City;
+    private String Picture;
+
+    public User(){
+
+    }
+
+    public void createAccount(String pseudo, String email, Date birthDate, String passWord, String gender, String city, String picture){
+
+    }
+
+    public void deleteAccount(){
+
+    }
+
+    public void modifyInformation(String pseudo, String email, Date birthDate, String passWord, String gender, String city, String picture){
+
+    }
+
+    public int getNumberUserPlant(){
+        return 0;
+    }
 }
